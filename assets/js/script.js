0358ad35f944105bae46244b06e4032b1f5fe138
dpprdgls <74167696+dpprdgls@users.noreply.github.com>
2023-08-23 15:31:30 -0700

    modify jscode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,19 @@
 // Assignment Code
 const generateBtn = document.querySelector("#generate");
 
-//variable for character amount 
-const charAmountRange = document.getElementById("charAmountRange");
-//variable for character amount number
-const charAmountNumberRange = document.getElementById("characterAmountNumber");
-//variable for upper case
-const charUpperCaseRange = document.getElementById("charUpperCaseRange");
-//variable for lower case
-const charLowerCaseRange = document.getElementById("charLowerCaseRange");
-//variable for special characters
-const charSymbolsCharacters = document.getElementById("charSpecialSymbols");
+
+//unnceccessary code from video
+
+// //variable for character amount 
+// const charAmountRange = document.getElementById("charAmountRange");
+// //variable for character amount number
+// const charAmountNumberRange = document.getElementById("characterAmountNumber");
+// //variable for upper case
+// const charUpperCaseRange = document.getElementById("charUpperCaseRange");
+// //variable for lower case
+// const charLowerCaseRange = document.getElementById("charLowerCaseRange");
+// //variable for special characters
+// const charSymbolsCharacters = document.getElementById("charSpecialSymbols");
 
 //change this
 const passwordDisplay = document.getElementById("passwordDisplay");
@@ -30,33 +33,61 @@ function writePassword() {
 
 }
 
-form.addEventListener("submit", event => {
-  event.preventDefault();
-  //the value needs to be changed to input of alert prompt
-  const charAmountRange = charAmountRange.value
-  const includeUppercase = includeUppercase.value
-  const includeLowercase = includeLowercase.value
-  const characterAmountNumber = characterAmountNumber.value
-  const includeSymbols = includeSymbols.value
-  const passwordgen = generatePassword(charAmountRange, characterAmountNumber, includeUppercase, includeLowercase, includeSymbols){
-    //set the id of the box to this field below to get output
-    passwordDisplay.innerText = passwordgen 
-  }
+//event listener from youtube video not needed here//
+
+// form.addEventListener("submit", event => {
+//   event.preventDefault();
+//   //the value needs to be changed to input of alert prompt
+//   const charAmountRange = charAmountRange.value
+//   const includeUppercase = includeUppercase.value
+//   const includeLowercase = includeLowercase.value
+//   const characterAmountNumber = characterAmountNumber.value
+//   const includeSymbols = includeSymbols.value
+//   const passwordgen = generatePassword(charAmountRange, characterAmountNumber, includeUppercase, includeLowercase, includeSymbols){
+//     //set the id of the box to this field below to get output
+//     passwordDisplay.innerText = passwordgen 
+//   }
 
   //generate password based on inputs and set arrays of characters to be used for password generation
 function generatePassword(charAmountRange, characterAmountNumber, includeUppercase, includeLowercase, includeSymbols){
-  let charCodes = LowerCaseCharCodes
-  if (includeUppercase) charCodes = charCodes.concat(UppercaseCharCodes)
-  if (characterAmountNumber) charCodes = charCodes.concat(NumberCharCodes)
-  if (includeSymbols) charCodes = charCodes.concat(SymbolsCharCodes)
-  const passwordCharacters = []
+  let charCodes = ""
 
-  for (let i = 0; i < characterAmount, i++){
+
+  let charAmountPrompt = prompt("Enter a password length between 8 and 128 characters");
+
+  if(charAmountPrompt < 8 || charAmountPrompt > 128){
+    alert("Please enter a valid password length");
+    return null;
+  }
+  
+  let includeLowercasePrompt = prompt("Type Y to include lowercase characters or N to exclude lowercase characters");
+
+  if (includeLowercasePrompt === 'Y') charCodes = charCodes.concat(LowerCaseCharCodes)
+
+  let includeUppercasePrompt = prompt("Type Y to include uppercase characters or N to exclude lowercase characters");
+
+  if (includeUppercasePrompt === 'Y') charCodes = charCodes.concat(UppercaseCharCodes)
+
+  let includeNumberPrompt = prompt("Type Y to include number characters or N to exclude number characters");
+
+  if (includeNumberPrompt === 'Y') charCodes = charCodes.concat(NumberCharCodes)
+
+  let includeSymbolsPrompt = prompt("Type Y to include symbols or N to exclude symbols");
+
+  if (includeSymbolsPrompt === 'Y') charCodes = charCodes.concat(SymbolsCharCodes)
+
+  if (includeLowercasePrompt !== 'Y' && includeUppercasePrompt !== 'Y' && includeNumberPrompt !== 'Y' && includeSymbolsPrompt !== 'Y'){
+    alert('Please enter at least one character type.');
+    return null;
+  } 
+
+  let passwordCharacters = []
+
+  for (let i = 0; i < charAmountPrompt, i++){
     const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
     passwordCharacters.push(String.fromCharCode(characterCode)
   }
-  return passwordCharacters.join('')
-
+  return passwordCharacters.join('');
 }
 
 function arrayFromLowToHigh(low, high){
@@ -71,11 +102,11 @@ function arrayFromLowToHigh(low, high){
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
+//unnecessary code from video 
 //event to set amount of characters and number of characters
-function setCharAmount(event){
-  const value = event.target.value
-  charAmountRange.value = value
-  characterAmountNumber.value = value
+// function setCharAmount(event){
+//   const value = event.target.value
+//   charAmountRange.value = value
+//   characterAmountNumber.value = value
 
-}
+// }
